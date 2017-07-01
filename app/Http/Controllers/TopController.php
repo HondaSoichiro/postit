500f0466d9fde57861e81cfc48af0ad6a85ccfce
Sou <suta1218@yahoo.co.jp>
2017-07-01 17:38:28 +0900

    スタート地点

---
+++ app/Http/Controllers/TopController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+
+	public function Top(){
+
+		return view('postit.top');
+
+	}
+
+    
+}
