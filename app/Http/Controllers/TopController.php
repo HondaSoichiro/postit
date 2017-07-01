e1195a842e16b952d5d4935c00624f6cb1d929cf
Yuto <yuto@honzannoMacBook-Pro.local>
2017-07-01 23:57:01 +0900

    デバッグ用

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 
 class TopController extends Controller
 {
@@ -10,8 +10,9 @@ class TopController extends Controller
 	public function Top(){
 
 		return view('postit.top');
+//		return view('welcome');
 
 	}
 
-    
+
 }
