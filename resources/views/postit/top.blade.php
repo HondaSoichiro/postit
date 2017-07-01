c32277c1ddf8a5070bb69cae37d5caa265bf48bb
Sou <suta1218@yahoo.co.jp>
2017-07-02 03:17:48 +0900

    Postitレイアウト(とりあえず)

--- resources/views/postit/top.blade.php
+++ resources/views/postit/top.blade.php
@@ -2,11 +2,25 @@
 @extends('postit.layout')
  
 @section('content')
-    <div>
-<form action="Res" method="post">
-<textarea name="sentence" rows="20" cols="40">ここに感想を記入してください。</textarea>
-<input type="submit" value="SEND">
-<input type="hidden" name="_token" value="{{csrf_token()}}">
-</form>
+
+<div class="paper">
+        <div class="sticky-input-field">
+             <form action="Res" method="post">
+                <textarea name="sentence" rows="20" cols="40" class="input-sticky-note">ここに感想を記入してください。</textarea>
+                <input type="submit" value="SEND">
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
+             </form>
+            </div>
+            <ul class="sticky-lines">
+                <li><span></span></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <div class="left-shadow"></div>
+            <div class="right-shadow"></div>
 </div>
 @endsection
