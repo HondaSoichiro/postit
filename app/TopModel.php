3f4468af35530f4c160adf6c36efbf118719e931
Sou <suta1218@yahoo.co.jp>
2017-07-02 12:20:59 +0900

    カラムがないなら新規作成ないなら更新

--- app/TopModel.php
+++ app/TopModel.php
@@ -8,6 +8,17 @@ class TopModel extends Model
 {
 	protected $table = 'postits';
 
+
+	protected $fillable = array('sentence'); //入力を許可するカラムの指定
+	protected $guarded = array('id'); // 入力を禁止するカラムの指定
 //	public $timestamps = false;
     //
 }
+
+// class Update extends Model
+// {
+
+// DB::table('postits')
+//             ->where('id', 1)
+//             ->update(['options->enabled' => true]);
+// }
