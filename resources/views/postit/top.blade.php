900ef8629220e0a84c4d9390d2e574a534f79715
Sou <suta1218@yahoo.co.jp>
2017-07-02 14:47:23 +0900

    PostiitデザインMark.1

--- resources/views/postit/top.blade.php
+++ resources/views/postit/top.blade.php
@@ -6,22 +6,9 @@
 <div class="paper">
         <div class="sticky-input-field">
              <form action="res" method="post">
-                <textarea name="sentence" rows="20" cols="40" class="input-sticky-note">{{$topmodel->sentence}}</textarea>
+                <textarea name="sentence" id="ta" rows="11" cols="40" class="input-sticky-note">{{$topmodel->sentence}}</textarea>
                 <input type="submit" value="SEND">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
              </form>
-            </div>
-            <ul class="sticky-lines">
-                <li><span></span></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <div class="left-shadow"></div>
-            <div class="right-shadow"></div>
-
 </div>
 @endsection
