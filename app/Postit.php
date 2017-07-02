9a7cf3a70b474a3832bd31df32e3114112322199
Yuto <yuto@honzannoMacBook-Pro.local>
2017-07-02 17:46:55 +0900

    topページのみでの処理 idを指定してのsave処理 modelへの関数記載

---
+++ app/Postit.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Postit extends Model {
+	protected $table = 'postits'; // postitsテーブルとの関連付け
+	protected $fillable = array ('sentence'); // 入力を許可するカラムの指定
+	protected $guarded = array ('id'); // 入力を禁止するカラムの指定
+
+	// IDとSentenceを受けて保存
+	public static function pi_save($id , $sentence) {
+		$postit = Postit::firstOrNew (['id'=> $id]); // インスタンス1つ
+//		$sentence = Input::get('sentence');
+		$postit->sentence = $sentence;
+		$postit->save ();
+		echo "保存完了";
+		return $postit;
+	}
+
+	// IDを受けて削除
+	public static function pi_delete($id) {
+		Postit::destroy($id);
+		return view('postit.top')->with ('postit',$postit);
+	}
+
+
+}
--- resources/views/postit/top.blade.php
+++ resources/views/postit/top.blade.php
@@ -5,8 +5,8 @@
 
 <div class="paper">
         <div class="sticky-input-field">
-             <form action="res" method="post">
-                <textarea name="sentence" id="ta" rows="11" cols="40" class="input-sticky-note">{{$topmodel->sentence}}</textarea>
+             <form action="5" method="post">
+                <textarea name="sentence" id="ta" rows="11" cols="40" class="input-sticky-note">{{$postit->sentence}}</textarea>
                 <input type="submit" value="SEND">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
              </form>
