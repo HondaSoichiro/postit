5db2e6026ecf82f1c708f0000e98319f784ffba1
Sou <suta1218@yahoo.co.jp>
2017-07-06 21:43:40 +0900

    クリックした位置の座標取得

--- resources/views/postit/top.blade.php
+++ resources/views/postit/top.blade.php
@@ -1,7 +1,7 @@
  @extends('postit.layout')
  @section('content')
 
-<!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript">
 $(function(){
     $('html').click(function(e){
@@ -11,26 +11,9 @@ $(function(){
         $('#locatedpoint').css({top:(y),left:(x),display:'block'}).attr('title','TOP : '+(y)+'px | LEFT : '+(x)+'px');
     });
 });
-</script> -->
-
-<script type="text/javascript">
-  var count;
-  function SetMark(x,y){
-    count++;
-    var element = document.createElement('p');
-    element.id = "id" + count;
-    element.innnerHTML = "●";
-    element.style.position = "absolute";
-    element.style.left = x + "px";
-    element.style.top = y + "px";
-    var obj = document.getElementById("cloneArea");
-    obj.appendChild(element);
-  }
 </script>
  @foreach ($postits as $postit) 
 
-<div class="cloneArea" onclick="SetMark(event.offsetX,event.offsetY)">
- @foreach ($postits as $postit)
 <div class="cloneArea">
 	<div class="paper" id="paper_origin">
 		<form action="1" method="post">
@@ -40,28 +23,7 @@ $(function(){
 		</form>
 	</div>
 </div>
-@endforeach
-
-<!-- 自動保存機能 -->
-<div id="div1022"></div>
-<script type="text/javascript">
-$(document).ready(function(){
-    $.PeriodicalUpdater({
-    //  オプション設定
-        url: '/1',      // 送信リクエストURL
-        minTimeout: 6000,    // 送信インターバル(ミリ秒)
-        method: 'get',      // 'post'/'get'：リクエストメソッド
-        sendData: $('form').serialize(),   // formの中身を全指定
-//      maxTimeout           // 最長のリクエスト間隔(ミリ秒)
-//      multiplier           // リクエスト間隔の変更(2に設定の場合、レスポンス内容に変更がないときは、リクエスト間隔が2倍になっていく)
-//      type                 // xml、json、scriptもしくはhtml (jquery.getやjquery.postのdataType)
-    },
-    function(data){
-        var myHtml = 'The data returned at ' + data + '';
-        $('#div1022').prepend(myHtml);
-    });
-})
-</script>
+@endforeach 
 @endsection
 
 
